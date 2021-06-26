4c4f6714ff758062aa50c966857bf32412f549d5
marrcelosantana <marcelosantana@alu.ufc.br>
2021-06-26 15:09:43 -0300

    First Commit

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { HomeComponent } from './_pages/home/home.component';
+import { ClientsComponent } from './_pages/clients/clients.component';
+import { CarsComponent } from './_pages/cars/cars.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'cars', component: CarsComponent },
+  { path: 'clients', component: ClientsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
