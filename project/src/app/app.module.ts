f309d60427b5ce9c9e644e98befce5c3751deb2e
marrcelosantana <marcelosantana@alu.ufc.br>
2021-06-26 00:44:58 -0300

    First Commit

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { CarsService } from './_services/cars.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,21 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarsComponent } from './_components/cars/cars.component';
 import { HeaderComponent } from './_components/header/header.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule}  from '@angular/material/input';
-import { InputsComponent } from './_components/cars/inputs/inputs.component';
 import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent,
-    HeaderComponent,
-    InputsComponent
+
   ],
   imports: [
     BrowserModule,
@@ -34,7 +29,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule
 
   ],
-  providers: [HttpClientModule, CarsService],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
